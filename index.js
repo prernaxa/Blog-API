8d61524ee61bee32b5fd1a811e74add5ea34e274
prernaxa <118099369+prernaxa@users.noreply.github.com>
2024-06-08 20:58:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,22 +38,22 @@ let lastId = 3;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Write your code here//
 
-//CHALLENGE 1: GET All posts
+
+//GET All posts
 
 app.get("/posts", (req, res) => {
   console.log(posts);
   res.json(posts);
 });
-//CHALLENGE 2: GET a specific post by id
+//GET a specific post by id
 
 app.get("/posts/:id", (req, res) => {
   const post = posts.find((p) => p.id === parseInt(req.params.id));
   if (!post) return res.status(404).json({ message: "Post not found" });
   res.json(post);
 });
-//CHALLENGE 3: POST a new post
+// POST a new post
 app.post("/posts", (req, res) => {
   const newId = lastId += 1;
   const post = {
@@ -69,7 +69,7 @@ app.post("/posts", (req, res) => {
 });
 
 
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
+// PATCH a post when you just want to update one parameter
 app.patch("/posts/:id", (req, res) => {
   const post = posts.find((p) => p.id === parseInt(req.params.id));
   if (!post) return res.status(404).json({ message: "Post not found" });
@@ -81,7 +81,7 @@ app.patch("/posts/:id", (req, res) => {
   res.json(post);
 });
 
-//CHALLENGE 5: DELETE a specific post by providing the post id.
+// DELETE a specific post by providing the post id.
 app.delete("/posts/:id", (req, res) => {
   const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
   if (index === -1) return res.status(404).json({ message: "Post not found" });
